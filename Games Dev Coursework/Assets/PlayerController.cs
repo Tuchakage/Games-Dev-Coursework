9bd329bf2460fe34ec12a0a158ee31fc46a11698
Tuchakage <62402636+Tuchakage@users.noreply.github.com>
2020-12-30 02:00:34 +0000

    Adding Character Movement, Camera, Character Model

---
+++ Games Dev Coursework/Assets/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    public Transform camera;
+
+    public float speed = 1.0f;
+
+    public float turnSmoothTime = 0.1f;
+
+    private float turnSmoothVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        //Check if we are moving in any direction
+        //.magnitude checks the length of our direction vector
+        if (direction.magnitude >= 0.1) 
+        {
+            //Getting the angle we want to use
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
+
+            // SmoothDampAngle(Current Angle, Target Angle, Reference to a variable that holds our current smooth velocity, Turn Smooth Time)
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            //Setting our Rotation
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            //Allows us to move in the direction of the Camera
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+
+
+    }
+}
---
+++ Games Dev Coursework/Assets/ThirdPersonCamera.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    public float mouseSensitivity = 10;
+    public bool lockCursor = true;
+    //Camera Rotation Of The X Axis Is called Pitch And Y Axis is Yaw
+    float pitch;
+    float yaw;
+    public Vector2 pitchMinMax = new Vector2(-3, 60);
+
+    public Transform target;
+    public float dstfromTarget = 2.0f;
+
+    public float rotationSmoothTime = 0.12f;
+    private Vector3 rotationSmoothVelocity;
+    private Vector3 currentRotation;
+
+    void Start() 
+    {
+        if (lockCursor) 
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+    // Update is called once per frame after all the other Update Methods
+    void LateUpdate()
+    {
+        yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
+        pitch -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+        //Clamping it so Camera cant do a full rotation
+        pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
+
+        
+
+        //Smoothing Camera Rotation
+        currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(pitch, yaw), ref rotationSmoothVelocity, rotationSmoothTime);
+
+        transform.eulerAngles = currentRotation;
+
+        //Makes the Camera look at the Target
+        transform.position = target.position - transform.forward * dstfromTarget;
+    }
+}
