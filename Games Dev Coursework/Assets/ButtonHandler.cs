1ae59ba0ca4e895d94bc123c55a8f0a6508e5945
Tuchakage <62402636+Tuchakage@users.noreply.github.com>
2020-12-30 20:44:00 +0000

    Simple Turn Based System

--- Games Dev Coursework/Assets/ButtonHandler.cs
+++ Games Dev Coursework/Assets/ButtonHandler.cs
@@ -5,19 +5,18 @@ using UnityEngine;
 public class ButtonHandler : MonoBehaviour
 {
     EnemyHealth eh;
+    TurnBasedSystem tbs;
 
-    //Disabling the player UI
-    public GameObject pui;
     private void Start()
     {
         eh = GameObject.Find("Enemy").GetComponent<EnemyHealth>();
+        tbs = GameObject.Find("TurnBasedSystem").GetComponent<TurnBasedSystem>();
     }
     public void attackButton() 
     {
         Debug.Log("Attack Button");
         //Everytime i press button take away -10
         eh.LoseHealth(10);
-
-        //pui.SetActive(false);
+        tbs.EnemyTurn();
     }
 }
--- Games Dev Coursework/Assets/TurnBasedSystem.cs
+++ Games Dev Coursework/Assets/TurnBasedSystem.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class TurnBasedSystem : MonoBehaviour
 {
+    //Disabling the player UI
+    public GameObject pui;
+
+    float Timer = 0;
+
+    bool enemyturn = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,35 @@ public class TurnBasedSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Timer > 0)
+        {
+            Timer -= Time.deltaTime;
+        }
+        else
+        {
+            if (enemyturn) 
+            {
+                PlayerTurn();
+                enemyturn = false;
+            }
+            
+        }
+
+    }
+
+    public void PlayerTurn() 
+    {
+        pui.SetActive(true);
+        Debug.Log("My Turn");
+    }
+
+    public void EnemyTurn() 
+    {
+        enemyturn = true;
+        pui.SetActive(false);
+        Debug.Log("Enemy Turn");
+
+        //After Enemy Animation then it goes back to player But For Now it is Timer based
+        Timer = 5;
     }
 }
